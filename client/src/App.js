9b8f9555e9bfbedba3bd276539a25b8456ce7619
belcherman2 <rlenihan@openadmin.com>
2022-12-28 13:30:53 -0500

    fixed App.js in client

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
           >
             Learn React!
           </a>
-          <Link to="/">Home</h1></Link>
+          <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
