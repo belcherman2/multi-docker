42ec65786511fcdce264a3269ba3abf1dc5a82bf
belcherman2 <rlenihan@openadmin.com>
2022-12-28 15:30:37 -0500

    fixed App.test.js again

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+      <div><h1>Fib Calculator</h1></div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>className="App-title">Welcome to React</h1>
