82d577ef1cd8335d92eb866d70c88633b6ec33bf
belcherman2 <rlenihan@openadmin.com>
2022-12-28 13:09:20 -0500

    chaged text on home page

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React!
           </a>
           <Link to="/"><h1>Fib Calculator</h1></Link>
           <Link to="/otherpage">Other Page</Link>
